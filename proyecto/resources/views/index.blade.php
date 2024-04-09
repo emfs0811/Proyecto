55dccdb4833ab8ea117bd1cfe082ebbedf8441a1
Erick Flores <erick.moises811@gmail.com>
2024-04-09 11:13:55 -0400

    se agrego la base del sistema

---
+++ proyecto/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="container" class=" col-xs-12">
+    <p><img src="img/ISOTIPO-BDV.png" width="55px" height="55px"> ARQFUN </p>
+                                <input type="text"  class=" col-xs-12" id="usuario" name="usuario" class="form-control input-lg" placeholder="NM / CT " autocomplete="off"><br>
+                                <input type="password"   class=" col-xs-12"id="clave" name="clave" class="form-control input-lg" placeholder="Contraseña" autocomplete="off"><br>
+                                <button type="submit" > Iniciar Sesión</button>
+</div>
+@endsection
---
+++ proyecto/resources/views/layouts/base.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="resources/css/app.css">
+    <title>proyecto</title>
+    
+</head>
+<body>
+   <div >
+        <div id="header">
+            <img class="imgheader" src="{{asset('img/CINTILLO-GOB-BOL-NEGATIVO.png')}}"width="1000px" height=100px>
+            <img src="{{ asset('img/cintilloportal.jpg') }}"width="100%" height=90px>
+        </div>
+        <div class="container">
+         @yield('content')
+        </div>
+        <div id="footer">
+        <footer>
+        <p><img class="imgfooter" src="{{asset('img/ISOTIPO-BDV.png')}}" width="25px" height="25px"> Banco de Venezuela - Gerencia de Línea Arquitectura Funcional-2022</p>
+        </footer> 
+        </div>
+    </div> 
+</body>
