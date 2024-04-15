5555ea2757417db03a166cdad3a881b3e6537b3a
Erick Flores <erick.moises811@gmail.com>
2024-04-15 12:37:34 -0400

    se creo el menu

--- proyecto/README.md
+++ proyecto/README.md
@@ -4,3 +4,4 @@
 4. *comando* "composer create-project laravel/laravel:^10.0 example-app"
 5. *comando* "cd carpeta"
 6. *comando* "php artisan serve"
+7. *comando* "npm install bootstrap"
--- proyecto/resources/views/index.blade.php
+++ proyecto/resources/views/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.base')
 @section('content')
+<link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js') }}" integrity="sha384-kenU1KFOnpDpg2onQQIiYzIJwAAMWcA8LdEOEHCvCWJh3zZcRQxYs5pJFnxSqOIk" crossorigin="anonymous"></script>
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
       <symbol id="check2" viewBox="0 0 16 16">
