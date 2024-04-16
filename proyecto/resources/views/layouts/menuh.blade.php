209f6fae3d1cabf0ff6338d17c2e274a9e9c9d3e
Erick Flores <erick.moises811@gmail.com>
2024-04-16 10:30:06 -0400

    Menu estatico

--- proyecto/resources/views/layouts/menuh.blade.php
+++ proyecto/resources/views/layouts/menuh.blade.php
@@ -9,13 +9,41 @@
     <nav>
         <div class="collapse navbar-collapse" id="menu">
             <ul>
+                <li>Inicio</li>
                 <li>Administracion</li>
                 <ul>
-                <li>Inicio</li>
+                <li>Menu</li>
                 <li>Usuarios</li>
+                <li>Roles</li>
                 </ul>
                 <li>Gerencia</li>
-                <li>Estadisticas</li>
+                <ul>
+                    <li>GASI</li>
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <li>GPC</li>
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <li>GEAV</li>
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        
+                    </ul>
+                    <li>GAPPS</li>
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                </ul>
                 <li>Auditoria</li>
                 <li>cerrar sesion</li>
             </ul>
